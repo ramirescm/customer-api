7adbe73cadef379416b3355ead89864786034c2b
Ramires <ramirescm@gmail.com>
2024-01-16 19:18:25 -0300

    feat: configurações gerais

--- src/Customer.API/Program.cs
+++ src/Customer.API/Program.cs
@@ -1,6 +1,73 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Customer.API;
+using Customer.API.Middlewares;
+using Customer.Application;
+using Customer.Core.Repositories;
+using Customer.Core.UoW;
+using Customer.Infra;
+using Customer.Infra.Repositories;
+using FluentValidation;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+ConfigureServices(builder);
+
+var app = builder.Build();
+ConfigureApp(app);
 
 app.Run();
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{
+    var services = builder.Services;
+    var configuration = builder.Configuration;
+
+    services.AddRouting(options => options.LowercaseUrls = true);
+    services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+    {
+        options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+    });
+
+    services.AddEndpointsApiExplorer();
+    services.AddTransient<ExceptionHandlingMiddleware>();
+    services.AddValidatorsFromAssemblyContaining<IApplicationEntryPoint>();
+    services.AddFluentValidationRulesToSwagger();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Title = "Customer.API",
+            Version = "v1",
+            Description = "Customer Crud"
+        });
+    });
+
+    services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("Customers"));
+
+    // TODO: if you want save data in postgresql
+    // AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    // services.AddDbContext<CustomerContext>(opt =>
+    //     opt.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseSettings") ?? throw new ArgumentException("DatabaseSettings not found")));
+
+    services.AddScoped<IUnitOfWork, UnitOfWork>();
+    services.AddScoped<ICustomerRepository, CustomerRepository>();
+    services.ConfigureAppDependencies(configuration);
+}
+
+static void ConfigureApp(WebApplication app)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(s =>
+    {
+        s.DocumentTitle = "AA";
+        s.DisplayRequestDuration();
+        s.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+        s.EnableDeepLinking();
+        s.ShowExtensions();
+        s.ShowCommonExtensions();
+    });
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+    app.UseHttpsRedirection();
+    app.MapEndpoints();
+}
